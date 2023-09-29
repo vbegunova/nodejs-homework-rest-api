ca2aeb4fb25180490c953f03d20914eb19144ca6
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-09-29 11:34:50 +0300

    replace schemas

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,30 +1,17 @@
 const express = require("express");
-const Joi = require("joi");
 
+const { HttpError } = require("../../helpers");
 const contacts = require("../../models/contacts");
+const {contactSchemas} = require("../../schemas")
 
 const router = express.Router();
-
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
-
-const updateSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
-}).or("name", "email", "phone");
 
 router.get("/", async (req, res, next) => {
   try {
     const result = await contacts.listContacts();
-    res.status(200).json({ data: result });
+    res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({
-      message: "Server error",
-    });
+    next(error);
   }
 });
 
@@ -33,34 +20,24 @@ router.get("/:contactId", async (req, res, next) => {
     const { contactId } = req.params;
     const result = await contacts.getContactById(contactId);
     if (!result) {
-      const error = new Error("Not Found");
-      error.status = 404;
-      throw error;
+      throw HttpError(404, "Not Found");
     }
-    res.status(200).json({ data: result });
+    res.status(200).json(result);
   } catch (error) {
-    const { status = 500, message = "Server error" } = error;
-    res.status(status).json({
-      message,
-    });
+    next(error);
   }
 });
 
 router.post("/", async (req, res, next) => {
   try {
-    const { error } = addSchema.validate(req.body);
+    const { error } = contactSchemas.addSchema.validate(req.body);
     if (error) {
-      const error = new Error("Missing required field or fields");
-      error.status = 400;
-      throw error;
+      throw HttpError(400, "Missing required field or fields");
     }
     const result = await contacts.addContact(req.body);
-    res.status(201).json({ data: result });
+    res.status(201).json(result);
   } catch (error) {
-    const { status = 500, message = "Server error" } = error;
-    res.status(status).json({
-      message,
-    });
+    next(error);
   }
 });
 
@@ -68,42 +45,29 @@ router.delete("/:contactId", async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const result = await contacts.removeContact(contactId);
-    console.log(result);
     if (!result) {
-      const error = new Error("Not Found");
-      error.status = 404;
-      throw error;
+      throw HttpError(404, "Not Found");
     }
     res.status(200).json({ message: "Contact deleted" });
   } catch (error) {
-    const { status = 500, message = "Server error" } = error;
-    res.status(status).json({
-      message,
-    });
+    next(error);
   }
 });
 
 router.put("/:contactId", async (req, res, next) => {
   try {
-    const { error } = updateSchema.validate(req.body);
+    const { error } = contactSchemas.updateSchema.validate(req.body);
     if (error) {
-      const error = new Error("Missing fields");
-      error.status = 400;
-      throw error;
+      throw HttpError(400, "Missing fields");
     }
     const { contactId } = req.params;
     const result = await contacts.updateContact(contactId, req.body);
     if (!result) {
-      const error = new Error("Not Found");
-      error.status = 404;
-      throw error;
+      throw HttpError(404, "Not Found");
     }
-    res.status(200).json({ data: result });
+    res.status(200).json(result);
   } catch (error) {
-    const { status = 500, message = "Server error" } = error;
-    res.status(status).json({
-      message,
-    });
+    next(error);
   }
 });
 
